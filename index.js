b9ae7478f6c15e40a78c62d9e15fba4d5dae434c
jeremyfourna <jeremyfourna@gmail.com>
2018-01-13 15:38:48 +0100

    WIP to display picker tour

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const R = require('ramda');
 
+const notNil = R.complement(R.isNil);
+const notEmpty = R.complement(R.isEmpty);
+
 const {
   shortestClosestNeighbourPath,
   shortestSShapedPath,
@@ -11,5 +14,41 @@ const el = document.getElementById('display-picker-tour');
 el.addEventListener('click', displayWarehouseAndPickerTour, false);
 
 function displayWarehouseAndPickerTour(event) {
-  console.log(event);
+  const formLens = R.lensPath(['target', 'form']);
+  const formData = R.view(formLens, event);
+
+  const data = R.mergeAll(R.map(cur => {
+    return notEmpty(R.prop('value', cur)) ? {
+      [R.prop('id', cur)]: R.prop('value', cur)
+    } : {};
+  }, formData));
+
+  console.log(data);
+  buildWarehouse(data);
+}
+
+function buildWarehouse(params) {
+  const warehouse = warehouseMatrix(Number(R.prop('nb-racks', params)), Number(R.prop('nb-aisles', params)), []);
+  console.log(warehouse);
+
+  const rows = wrapRow(R.map(buildRow, warehouse));
+
+  render('app', rows);
+}
+
+function wrapRow(rows) {
+  return `<div class="warehouse">${R.join('', rows)}</div>`;
+}
+
+function buildRow(list) {
+  function typeOfPlace(place) {
+    return R.equals(place, 0) ? `<div class="place"></div>` : `<div class="place wall"></div>`;
+  }
+
+  return `<div class="row">${R.join('', R.map(typeOfPlace, list))}</div>`;
+}
+
+function render(domId, content) {
+  const el = document.getElementById(domId).innerHTML = content;
+  return el;
 }
